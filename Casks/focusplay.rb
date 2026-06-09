370a6d77927ca97914d3b1d332f0939cad67cd97
oldpiano <oldpiano@upstairscorp.com>
2026-06-09 10:57:12 +0900

    Add focusplay cask 1.0.1

---
+++ Casks/focusplay.rb
@@ -0,0 +1,16 @@
+cask "focusplay" do
+  version "1.0.1"
+  sha256 "7869d4849919a79e076fc7f786ef43a4202c12cb27633ed8d0ce2212a290662b"
+
+  url "https://github.com/TypoStudio/focus-play/releases/download/v#{version}/FocusPlay-#{version}.zip"
+  name "FocusPlay"
+  desc "Multi-monitor focus mode menu bar app"
+  homepage "https://github.com/TypoStudio/focus-play"
+
+  auto_updates false
+  depends_on macos: ":ventura"
+
+  app "FocusPlay.app"
+
+  zap trash: "~/Library/Preferences/com.typostudio.focusplay.plist"
+end
