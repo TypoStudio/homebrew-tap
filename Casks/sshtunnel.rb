4ea96b028108cbe74c68f3b3eb7475882ffc3a4f
oldpiano <oldpiano@gmail.com>
2026-09-16 10:03:00 +0900

    sshtunnel 1.6.1

--- Casks/sshtunnel.rb
+++ Casks/sshtunnel.rb
@@ -1,6 +1,6 @@
 cask "sshtunnel" do
-  version "1.6.0"
-  sha256 "4821c117982b7955cdebb17d3508333dbb181dfa7b215c733d0b1c71de6fe190"
+  version "1.6.1"
+  sha256 "3fa11b95a6a7893548cdf26dae0e951526737d810085a655cc181dc35d36fd36"
 
   url "https://github.com/TypoStudio/ssh-tunnel-for-macos/releases/download/v#{version}/SSHTunnel-#{version}.dmg"
   name "SSHTunnel"
